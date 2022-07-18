45e510f2a97a179e79619a6a28d84dba6fa2afea
Abhi <abhisea12@gmail.com>
2022-07-18 10:51:09 -0700

    This is my message

---
+++ Java/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    /* 
+     * Every line of code that runs in Java must be inside a class
+     * A class should always start with an uppercase first letter.
+     * Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+     * The name of the java file must match the class name. 
+     * When saving the file, save it using the class name and add ".java" to the end of the filename. 
+     * The main() method is required and you will see it in every Java program:
+     * 
+    */
+}

687d79c10e9d7b8326eba89df708fa3102283239
Abhi <abhisea12@gmail.com>
2022-07-18 10:49:29 -0700

    edits

---
+++ Java/src/VariablesInfo.java
@@ -0,0 +1,11 @@
+public class VariablesInfo {
+    public static void main(String[] args){
+        String name = "John";
+        System.out.println(name);
+
+        /*
+         * Where type is one of Java's types (such as int or String), 
+         * and variable is the name of the variable (such as x or name). 
+         */
+    }
+}
